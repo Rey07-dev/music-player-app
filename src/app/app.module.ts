1280466d28367b0c40ca681ea6d4b0d0596d22c9
Reuben Ekow Yawson <87475624+Reuben-PropsDev@users.noreply.github.com>
2024-08-01 09:16:24 +0000

    feat: add packages

---
+++ src/app/app.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from "@angular/core";
+import { StoreModule } from "@ngrx/store";
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    StoreModule.forRoot({}) //add the reducers here
+  ],
+  exports: []
+})
+export class AppModule { }
