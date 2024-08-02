bd4ee3e36949b63bb2e846e37779d18db28c60a0
Reuben Ekow Yawson <87475624+Reuben-PropsDev@users.noreply.github.com>
2024-08-02 14:11:46 +0000

    feat: import modules and route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,30 @@
-import { NgModule } from "@angular/core";
-import { StoreModule } from "@ngrx/store";
-
+import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
+import { HomeComponent } from './pages/home/home.component';
+import { AppComponent } from './app.component';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutes } from './app.routes';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatButtonModule } from '@angular/material/button';
+import { themeReducer } from './store/theme/theme.reducer';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [AppComponent, HomeComponent],
   imports: [
-    StoreModule.forRoot({}) //add the reducers here
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterOutlet,
+    RouterModule,
+    RouterModule.forRoot(AppRoutes),
+    StoreModule.forRoot({ theme: themeReducer }),
+    SharedModule,
+    MatButtonModule,
   ],
-  exports: []
+  providers: [provideAnimationsAsync()],
+  bootstrap: [AppComponent],
+  exports: [],
 })
-export class AppModule { }
+export class AppModule {}
