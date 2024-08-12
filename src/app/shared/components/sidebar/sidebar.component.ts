6512f6a89083cfa25c22e46eec60efa2db5d39a1
Reuben Ekow Yawson <87475624+Reuben-PropsDev@users.noreply.github.com>
2024-08-12 11:00:19 +0000

    feat: add the functionalities and UI

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,10 +1,14 @@
+import { MusicList } from '../../../core/models/interfaces/music/types';
+import { musicList, libraryList } from './../../../core/constants/musicContants';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+  styleUrl: './sidebar.component.css',
 })
 export class SidebarComponent {
-
+ musicList: MusicList[] = musicList
+ libraryList: MusicList[] = libraryList
 }
