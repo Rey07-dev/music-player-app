e09e5abffc854b2601f4bc4e30bf74d44e320080
Reuben Ekow Yawson <87475624+Reuben-PropsDev@users.noreply.github.com>
2024-08-12 11:55:56 +0000

    feat: add card

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Track } from '../../../core/models/interfaces/music/tracks';
+import { PlaybackService } from '../../../core/models/services/play/playback.service';
+import { selectTrack } from '../../../store/play-track/playMusic.actions';
 
 @Component({
   selector: 'app-card',
@@ -6,5 +10,16 @@ import { Component } from '@angular/core';
   styleUrl: './card.component.css'
 })
 export class CardComponent {
+  @Input({ required: true }) songs!: Track;
+  placeholder: string = "astro.png";
+  showPlayButton = false;
 
+  constructor(private playbackService: PlaybackService, private store: Store) {}
+
+  playSong(song: Track) {
+    // console.log(song);
+    // this.playbackService.getTrackInfo(song);
+    // this.playbackService.playTrack(parseInt(song.intDuration), song)
+    this.store.dispatch(selectTrack({ track: song }));
+  }
 }
