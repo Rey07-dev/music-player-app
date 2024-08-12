8d79893925174ed042d2d91112d539ce5ace7f85
Reuben Ekow Yawson <87475624+Reuben-PropsDev@users.noreply.github.com>
2024-08-12 12:11:21 +0000

    feat: add carousel component

---
+++ src/app/core/constants/slide.ts
@@ -0,0 +1,29 @@
+import { Slide } from "../models/interfaces/types";
+
+export const slides: Slide[] = [
+  {
+    image: 'm1.webp',
+    title: 'Till Morning Comes',
+    subtitle: 'Maddy Maznaz ft. Bisa Kdei'
+  },
+  {
+    image: 'm2.jpg',
+    title: 'Another Song',
+    subtitle: 'Artist Name'
+  },
+  {
+    image: 'm3.jpg',
+    title: 'Another Song',
+    subtitle: 'Artist Name'
+  },
+  {
+    image: 'm4.jpg',
+    title: 'Another Song',
+    subtitle: 'Artist Name'
+  },
+  {
+    image: 'm5.webp',
+    title: 'Another Song',
+    subtitle: 'Artist Name'
+  }
+];
--- src/app/shared/components/home-carousel/home-carousel.component.ts
+++ src/app/shared/components/home-carousel/home-carousel.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Slide } from "../../../core/models/interfaces/types";
+import { slides } from "../../../core/constants/slide";
 
 @Component({
-  selector: 'app-home-carousel',
-  templateUrl: './home-carousel.component.html',
-  styleUrl: './home-carousel.component.css'
+  selector: "app-home-carousel",
+  templateUrl: "./home-carousel.component.html",
+  styleUrl: "./home-carousel.component.css",
 })
 export class HomeCarouselComponent {
 
+  slides = slides;
+  currentIndex = 0;
+  intervalId: any;
+
+  ngOnInit() {
+    this.startAutoSlide();
+  }
+
+  ngOnDestroy() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
+
+  startAutoSlide() {
+    this.intervalId = setInterval(() => {
+      this.nextSlide();
+    }, 5000);
+  }
+
+  prevSlide() {
+    this.currentIndex =
+      this.currentIndex > 0 ? this.currentIndex - 1 : this.slides.length - 1;
+  }
+
+  nextSlide() {
+    this.currentIndex =
+      this.currentIndex < this.slides.length - 1 ? this.currentIndex + 1 : 0;
+  }
+
+  goToSlide(index: number) {
+    this.currentIndex = index;
+  }
 }
