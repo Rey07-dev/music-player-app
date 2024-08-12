d22d8679b877a3705266797ab33b32ebc22a9cee
Reuben Ekow Yawson <87475624+Reuben-PropsDev@users.noreply.github.com>
2024-08-12 11:48:15 +0000

    feat: add trucks and carousel

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Track, Tracks } from '../../core/models/interfaces/music/tracks';
+import { MusicService } from '../../core/models/services/music.service';
+import { AlbumState } from '../../store/musicAlbum/album.state';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +10,21 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  // songs: Top50Songs[] = this.songsService.getTopSongs();
+  tracks!: Track[];
+
+
+  constructor(private store: Store<{ albums: AlbumState }>,
+    // private songsService: TopSongsService,
+    private musicService: MusicService
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.musicService.getTracks('2115886').subscribe((tracks: Tracks) => {
+      console.log(tracks);
+      this.tracks = tracks.track;
+    });
+  }
 
 }
