0c14101f8dcb54189d88a39dcc66f8324e1c28a8
Reuben Ekow Yawson <87475624+Reuben-PropsDev@users.noreply.github.com>
2024-08-12 10:49:07 +0000

    feat: add list menu for sidebar

---
+++ src/app/core/constants/musicContants.ts
@@ -0,0 +1,56 @@
+import { MusicList } from '../models/interfaces/music/types';
+
+export const musicList: MusicList[] = [
+  {
+    name: 'Home',
+    icon: 'fas fa-user-astronaut',
+    link: '',
+    label: 'Home',
+  },
+  {
+    name: 'Trending',
+    label: 'Trending',
+    icon: 'fas fa-fire',
+    link: '/trending',
+  },
+  // {
+  //   name: 'New',
+  //   icon: 'fas fa-music',
+  //   link: '/new-releases',
+  //   label: 'New Releases',
+  //   isActive: false
+  // },
+  {
+    name: 'Artists',
+    icon: 'fas fa-user-md',
+    link: '/artists',
+    label: 'Artists',
+  },
+  {
+    name: 'Albums',
+    icon: 'fa fa-heart-music-camera-bolt',
+    link: '/albums',
+    label: 'Albums',
+  },
+  {
+    name: 'Genres',
+    icon: 'fas fa-compact-disc',
+    link: '/genres',
+    label: ' Genres',
+  },
+];
+
+export const libraryList: MusicList[] = [
+  {
+    name: 'Favorites',
+    icon: 'fas fa-heart',
+    link: '/favorites',
+    label: 'Favorites',
+  },
+  {
+    name: 'My Playlists',
+    icon: 'fas fa-list',
+    link: '/playlists',
+    label: 'My Playlist',
+  },
+];
