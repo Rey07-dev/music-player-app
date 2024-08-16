51a43ad8c538fe2002455696ba0a69fd56dba015
Reuben Ekow Yawson <87475624+Reuben-PropsDev@users.noreply.github.com>
2024-08-16 11:27:57 +0000

    feat: add player controls constant variables

--- src/app/core/constants/slide.ts
+++ src/app/core/constants/slide.ts
@@ -2,28 +2,35 @@ import { Slide } from "../models/interfaces/types";
 
 export const slides: Slide[] = [
   {
-    image: 'm1.webp',
-    title: 'Till Morning Comes',
-    subtitle: 'Maddy Maznaz ft. Bisa Kdei'
+    image: "m1.webp",
+    title: "Till Morning Comes",
+    subtitle: "Maddy Maznaz ft. Bisa Kdei",
   },
   {
-    image: 'm2.jpg',
-    title: 'Another Song',
-    subtitle: 'Artist Name'
+    image: "m2.jpg",
+    title: "Another Song",
+    subtitle: "Artist Name",
   },
   {
-    image: 'm3.jpg',
-    title: 'Another Song',
-    subtitle: 'Artist Name'
+    image: "m3.jpg",
+    title: "Another Song",
+    subtitle: "Artist Name",
   },
   {
-    image: 'm4.jpg',
-    title: 'Another Song',
-    subtitle: 'Artist Name'
+    image: "m4.jpg",
+    title: "Another Song",
+    subtitle: "Artist Name",
   },
   {
-    image: 'm5.webp',
-    title: 'Another Song',
-    subtitle: 'Artist Name'
-  }
+    image: "m5.webp",
+    title: "Another Song",
+    subtitle: "Artist Name",
+  },
 ];
+
+export const playerControl = {
+  play: "/play",
+  pause: "/pause",
+  next: "/next",
+  previous: "/previous",
+};
