c048bbf55cccb798dffea7d2d7d3e5b23e8f11a7
Reuben Ekow Yawson <87475624+Reuben-PropsDev@users.noreply.github.com>
2024-08-16 11:35:52 +0000

    feat: add service for songs player and controls

--- src/app/core/models/services/play/playback.service.ts
+++ src/app/core/models/services/play/playback.service.ts
@@ -1,79 +1,51 @@
 import { Injectable } from "@angular/core";
 import { BehaviorSubject } from "rxjs/internal/BehaviorSubject";
-import { Track } from "../../interfaces/music/tracks";
-import { SpotifyTrack } from "../../interfaces/spotify";
+import { IAlbumItem } from "../../interfaces/spotify";
+import { SpotifyPlayerService } from "../spotify/spotify-player.service";
 
 @Injectable({
   providedIn: "root",
 })
 export class PlaybackService {
   private currentTrackSubject = new BehaviorSubject<any>(null);
-  public playlist: any[] = [];
-  public currentIndex = -1;
-  private playbackPosition = 0;
+  private currentIndexSubject = new BehaviorSubject<number>(-1);
+  public playlist: IAlbumItem[] = [];
   audio = new Audio();
 
   currentTrack$ = this.currentTrackSubject.asObservable();
+  currentIndex$ = this.currentIndexSubject.asObservable();
   playbackPosition$ = new BehaviorSubject<number>(0);
 
-  setPlaylist(tracks: SpotifyTrack[]) {
+  constructor(private spotifyService: SpotifyPlayerService,) { }
+
+  setPlaylist(tracks:IAlbumItem[]) {
     this.playlist = tracks;
-    this.currentIndex = -1;
+    this.currentIndexSubject.next(-1);
   }
 
   playTrack(index: number) {
     const track = this.playlist[index];
-    const audio = this.audio;
-    audio.src = track.external_urls.spotify;
-
-    audio.oncanplay = () => {
-      audio
-        .play()
-        .catch((error) => console.error("Error playing track:", error));
-    };
-    audio.load();
-    // console.log(index, 'index');
-    if (index >= 0 && index < this.playlist.length) {
-      this.currentIndex = index;
-      const track = this.playlist[index];
-      this.currentTrackSubject.next(track);
-      this.audio.src = track.external_urls.spotify;
-      this.audio.play();
-      this.playbackPosition = 0;
-      this.playbackPosition$.next(this.playbackPosition);
-      console.log(`Playing track: ${track.name}`);
-    }
+    this.spotifyService.play(track?.uri);
+    this.currentIndexSubject.next(index);
+    this.currentTrackSubject.next(track);
   }
 
   pauseTrack() {
-    if (this.currentTrackSubject.value) {
-      console.log(`Pausing track: ${this.currentTrackSubject.value.name}`);
-      this.audio.pause();
-    }
+    this.spotifyService.pause();
   }
 
-  seek(position: number) {
-    if (this.currentTrackSubject.value) {
-      this.playbackPosition = position;
-      this.playbackPosition$.next(this.playbackPosition);
-      this.audio.currentTime = position;
-      console.log(`Seeked to position: ${position}`);
-    }
-  }
 
   nextTrack() {
-    if (this.currentIndex < this.playlist.length - 1) {
-      this.playTrack(this.currentIndex + 1);
+    const currentIndex = this.currentIndexSubject.value;
+    if (currentIndex < this.playlist.length - 1) {
+      this.playTrack(currentIndex + 1);
     }
   }
 
   previousTrack() {
-    if (this.currentIndex > 0) {
-      this.playTrack(this.currentIndex - 1);
+    const currentIndex = this.currentIndexSubject.value;
+    if (currentIndex > 0) {
+      this.playTrack(currentIndex - 1);
     }
-  }
-
-  trackFinished() {
-    this.nextTrack();
   }
 }
--- src/app/core/models/services/spotify/spotify-player.service.ts
+++ src/app/core/models/services/spotify/spotify-player.service.ts
@@ -1,58 +1,154 @@
-import { Injectable } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { SpotifyState } from '../../../../store/spotify/spotify.reducer';
+import { Injectable } from "@angular/core";
+import { PlayingSongState } from "../../interfaces/spotify";
+import { Store } from "@ngrx/store";
+import { environment } from "../../../../../environments/environment";
+import { playerControl } from "../../../constants/slide";
 
-// declare global {
-//   interface Window {
-//     onSpotifyWebPlaybackSDKReady: () => void;
-//     Spotify: any;
-//   }
-// }
+declare global {
+  interface Window {
+    onSpotifyWebPlaybackSDKReady: () => void;
+    Spotify: typeof Spotify;
+  }
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class SpotifyPlayerService {
-
   player: any;
+  device_id!: string;
+  public isPaused: boolean = true;
 
-  // constructor(private store: Store<{ spotify: SpotifyState }>) {
-  //   this.store.select('spotify').subscribe((state) => {
-  //     if (state.token) {
-  //       this.initializePlayer(state.token);
-  //     }
-  //   });
-  // }
+  constructor(private store: Store<{ playingSong: PlayingSongState }>) {
+    this.loadSpotifySDK();
+  }
 
-  // initializePlayer(token: string) {
-  //   window.onSpotifyWebPlaybackSDKReady = () => {
-  //     this.player = new window.Spotify.Player({
-  //       name: 'Angular Spotify Player',
-  //       getOAuthToken: (cb: any) => {
-  //         cb(token);
-  //       },
-  //       volume: 0.5,
-  //     });
+  loadSpotifySDK() {
+    if (!window.Spotify) {
+      const script = document.createElement("script");
+      script.src = "https://sdk.scdn.co/spotify-player.js";
+      script.onload = () => {
+        this.initializePlayer();
+      };
+      document.body.appendChild(script);
+    } else {
+      this.initializePlayer();
+    }
+  }
 
-  //     this.player.connect();
-  //   };
+  initializePlayer() {
+    const token = localStorage.getItem("spotify_token");
 
-  //   if (!window.Spotify) {
-  //     const script = document.createElement('script');
-  //     script.src = 'https://sdk.scdn.co/spotify-player.js';
-  //     document.body.appendChild(script);
-  //   }
-  // }
+    if (!token) {
+      console.error("Spotify token not found in localStorage.");
+      return;
+    }
 
-  // play(uri: string) {
-  //   console.log(uri,localStorage.getItem('spotifyToken'));
-  //   fetch(`https://api.spotify.com/v1/me/player/play`, {
-  //     method: 'PUT',
-  //     body: JSON.stringify({ uris: [uri] }),
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //       Authorization: `Bearer ${localStorage.getItem('spotifyToken')}`,
-  //     },
-  //   });
-  // }
+    window.onSpotifyWebPlaybackSDKReady = () => {
+      this.player = new window.Spotify.Player({
+        name: "Angular Spotify Player",
+        getOAuthToken: (cb: any) => {
+          cb(token);
+        },
+        volume: 0.5,
+      });
+
+      this.player.addListener("ready", ({ device_id }: any) => {
+        this.device_id = device_id;
+        localStorage.setItem("device_id", device_id);
+      });
+
+      this.player.addListener("player_state_changed", (state: any) => {
+        if (!state) {
+          return;
+        }
+        this.isPaused = state.paused;
+        localStorage.setItem("player_state", JSON.stringify(state));
+      });
+
+      this.player.connect();
+    };
+
+    if (window.Spotify && window.onSpotifyWebPlaybackSDKReady) {
+      window.onSpotifyWebPlaybackSDKReady();
+    }
+  }
+
+  play(uri: string, playlists?: string[]) {
+    if (!this.player) {
+      console.error("Player not initialized");
+      return;
+    }
+
+    const token = localStorage.getItem("spotify_token");
+    const playData = {
+      context_uri: uri,
+      uris: playlists,
+      offset: {
+        position: 0,
+      },
+      position_ms: 0,
+    };
+    const deviceId = this.device_id
+      ? this.device_id
+      : localStorage.getItem("device_id");
+    fetch(
+      `${environment.playerURL}${playerControl.play}?device_id=${deviceId}`,
+      {
+        method: "PUT",
+        body: JSON.stringify(playData),
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    ).then();
+  }
+
+  pause() {
+    const deviceId = this.device_id || localStorage.getItem("device_id");
+    const token = localStorage.getItem("spotify_token");
+    fetch(
+      `${environment.playerURL}${playerControl.pause}?device_id=${deviceId}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    ).then();
+  }
+
+  next() {
+    const deviceId = this.device_id || localStorage.getItem("device_id");
+    const token = localStorage.getItem("spotify_token");
+
+    fetch(
+      `${environment.playerURL}${playerControl.next}?device_id=${deviceId}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    ).then();
+  }
+
+  previous() {
+    const deviceId = this.device_id || localStorage.getItem("device_id");
+    const token = localStorage.getItem("spotify_token");
+
+    fetch(
+      `${environment.playerURL}${playerControl.previous}?device_id=${deviceId}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    ).then();
+  }
 }
