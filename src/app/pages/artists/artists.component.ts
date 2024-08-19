9b6e6105b63410bdfa3406792b992b0dd17d2c9e
Reuben Ekow Yawson <87475624+Reuben-PropsDev@users.noreply.github.com>
2024-08-19 09:50:45 +0000

    refactor: update the these component

--- src/app/pages/artists/artists.component.ts
+++ src/app/pages/artists/artists.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SpotifyService } from '../../core/models/services/spotify/spotify.service';
+import { Artist } from '../../core/models/interfaces/user/artist';
 
 @Component({
   selector: 'app-artists',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrl: './artists.component.css'
 })
 export class ArtistsComponent {
+  artist?: Artist;
+  constructor(private spotifyService: SpotifyService) { }
 
+  ngOnInit(): void {
+    this.spotifyService.getSingleArtist().subscribe({
+      next: (data) => {
+        this.artist = data.artists[0]
+      },
+      error: (err) => {
+        console.log(err)
+      }
+    })
+  }
 }
