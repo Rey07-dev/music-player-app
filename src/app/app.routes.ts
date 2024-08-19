a7d0995cff265d3fb19d6e4ecce7dc7420ee9a19
Reuben Ekow Yawson <87475624+Reuben-PropsDev@users.noreply.github.com>
2024-08-19 09:47:53 +0000

    feat: add genre and single album routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,13 +6,17 @@ import { TrendingComponent } from './pages/trending/trending.component';
 import { LoginComponent } from './shared/components/login/login.component';
 import { CallbackComponent } from './shared/components/login/callback.component';
 import { AuthGuard } from './core/models/guards/auth.guard';
+import { TracksInAlbumComponent } from './shared/components/tracks-in-album/tracks-in-album.component';
+import { GenreComponent } from './pages/genre/genre.component';
 
 export const AppRoutes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full', canActivate: [AuthGuard] },
   { path: 'albums', component: AlbumsComponent, canActivate: [AuthGuard] },
+  { path: 'album/:id', component: TracksInAlbumComponent, canActivate: [AuthGuard] },
   { path: 'artists', component: ArtistsComponent, canActivate: [AuthGuard] },
   { path: 'trending', component: TrendingComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
+  { path: 'genres', component: GenreComponent, canActivate: [AuthGuard] },
   { path: 'callback', component: CallbackComponent },
   { path: '**', redirectTo: '' },
 
