52c066e48999432744f428c50e29110a5686e057
Reuben Ekow Yawson <87475624+Reuben-PropsDev@users.noreply.github.com>
2024-08-21 12:07:15 +0000

    refactor: refactor the code

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,6 +2,9 @@ import { Component } from "@angular/core";
 import { SpotifyPlayerService } from "../../core/models/services/spotify/spotify-player.service";
 import { SpotifyService } from "../../core/models/services/spotify/spotify.service";
 import { IAlbumItem } from "../../core/models/interfaces/spotify";
+import { SpotifyGenreData } from "../../core/models/interfaces/genre";
+import { Router } from "@angular/router";
+import { ToastService } from "../../core/models/services/toast/toast.service";
 
 @Component({
   selector: "app-home",
@@ -10,23 +13,58 @@ import { IAlbumItem } from "../../core/models/interfaces/spotify";
 })
 export class HomeComponent {
   tracks!: IAlbumItem[];
+  genreData!: SpotifyGenreData;
 
   constructor(
     private spotifyPlayerService: SpotifyPlayerService,
-    private spotifyService: SpotifyService
+    private spotifyService: SpotifyService,
+    private route: Router,
+    private toastService: ToastService
   ) {}
 
   ngOnInit(): void {
+    this.getNewReleasesAlbum();
+    this.getGenres();
+  }
+
+  getNewReleasesAlbum() {
     this.spotifyService.getNewReleases().subscribe({
       next: (data) => {
         if (data) {
           this.tracks = data.albums.items;
         }
       },
+      error(err) {
+        console.error(err);
+      },
+    });
+
+  }
+
+  getGenres(){
+    this.spotifyService.getGenre().subscribe({
+      next: (data) => {
+        this.genreData = data;
+      },
+      error: (err) => {
+        this.toastService.showToast(
+          "Error fetching genre data",
+          "error",
+        )
+      },
     });
   }
 
-  pauseTrack() {
-    this.spotifyPlayerService.player.pause();
+  playPlaylist(playlist: any) {
+    localStorage.setItem("album", JSON.stringify(playlist));
+    this.spotifyPlayerService.play(playlist.uri);
+  }
+
+  goToGenres() {
+    this.route.navigate(['/genres'])
+  }
+
+  goToAlbums() {
+    this.route.navigate(['/albums'])
   }
 }
