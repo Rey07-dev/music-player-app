37fd2a315f35972da88318434f2cd7c78a93968d
Reuben Ekow Yawson <87475624+Reuben-PropsDev@users.noreply.github.com>
2024-08-21 13:18:02 +0000

    feat: add env file

---
+++ src/environments/set.env.ts
@@ -0,0 +1,35 @@
+export const setEnv = () => {
+  const fs = require("fs");
+  const writeFile = fs.writeFile;
+  // Configure Angular `environment.ts` file path
+  const targetPathProd = "./src/environments/environment.ts";
+  // const targetPathDev = "./src/environments/environment.development.ts";
+  // Load node modules
+  const appVersion = require("../../package.json").version;
+  require("dotenv").config({
+    path: "src/environments/.env",
+  });
+
+  const envConfigFileProd = `export const environment = {
+    xAPN: '${process.env["xAPN"]}'
+    innoBasicUrl: '${process.env["innoBasicUrl"]}'
+    version: '${appVersion}'
+    apiBasicUrl: '${process.env["apiBasicUrl"]}'
+    apiKey: '${process.env["apiKey"]}'
+    clientId: '${process.env["clientId"]}'
+    clientSecret: '${process.env["clientSecret"]}'
+    redirectUri: '${process.env["redirectUri"]}'
+    authEndpoint: '${process.env["authEndpoint"]}'
+    scopes: '${process.env["scopes"]}'
+    spotify_Token: '${process.env["spotify_Token"]}'
+    playerURL: '${process.env["playerURL"]}'
+    script_for_player: '${process.env["script_for_player"]}'
+    searchURL: '${process.env["searchURL"]}'
+  };`;
+
+  writeFile(targetPathProd, envConfigFileProd, () => {
+    console.log('Astro config file created!');
+  });
+};
+
+setEnv()
