b4e9e663babda725414c2a28ebb9342a5b9e6f63
Reuben Ekow Yawson <87475624+Reuben-PropsDev@users.noreply.github.com>
2024-08-21 12:54:01 +0000

    refactor: refactor the code

--- src/app/core/interceptors/spotify.interceptor.ts
+++ src/app/core/interceptors/spotify.interceptor.ts
@@ -1,43 +1,47 @@
-import { AuthService } from './../models/services/spotify/auth.service';
+import { SpotifyAuthService } from "./../models/services/spotify/auth.service";
 import {
   HttpEvent,
   HttpRequest,
   HttpErrorResponse,
   HttpHandlerFn,
 } from "@angular/common/http";
-import { inject } from '@angular/core';
+import { inject } from "@angular/core";
 import { Observable, throwError } from "rxjs";
 import { catchError, switchMap } from "rxjs/operators";
+import { environment } from "../../../environments/environment";
 
 export function spotifyPlayerInterceptor(
   req: HttpRequest<any>,
   next: HttpHandlerFn
 ): Observable<HttpEvent<any>> {
   const token = localStorage.getItem("spotify_token");
-  const authService = inject(AuthService);
+  const spotifyAuthService = inject(SpotifyAuthService);
+  const clientId = environment.clientId;
+  const clientSecret = environment.CLIENT_SECRET;
 
   let clonedRequest = req.clone({
-    headers: req.headers.set("Authorization", `Bearer ${token}`,),
-  });;
+    headers: req.headers.set("Authorization", `Bearer ${token}`),
+  });
 
   return next(clonedRequest).pipe(
     catchError((error: HttpErrorResponse) => {
       if (error.status === 401) {
-        return authService.refreshAccessToken().pipe(
+        return spotifyAuthService.refreshAccessToken().pipe(
           switchMap((response) => {
             const newToken = response.access_token;
-            localStorage.setItem('spotify_token', newToken);
+            localStorage.setItem("spotify_token", newToken);
             const newRequest = req.clone({
               headers: req.headers.set("Authorization", `Bearer ${newToken}`),
             });
             return next(newRequest);
           }),
           catchError((refreshError) => {
-            console.error('Refresh token failed', refreshError);
-            authService.logout();
+            spotifyAuthService.logout();
             return throwError(() => refreshError);
           })
         );
+      } else if (error.status === 400) {
+        return handleUnauthorized(req, next, clientId, clientSecret);
       } else {
         return throwError(() => error);
       }
@@ -47,16 +51,15 @@ export function spotifyPlayerInterceptor(
 
 function handleUnauthorized(
   req: HttpRequest<any>,
-  next: HttpHandlerFn, accessToken: string
+  next: HttpHandlerFn,
+  clientId: string,
+  clientSecret: string
 ): Observable<HttpEvent<any>> {
-  if (accessToken) {
-    const clonedRequest = req.clone({
-      setHeaders: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
-    return next(clonedRequest);
-  } else {
-    return throwError(() => new Error("Unauthorized: Token not found"));
-  }
+  const clonedRequest = req.clone({
+    setHeaders: {
+      Authorization: "Basic " + btoa(clientId + ":" + clientSecret),
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  });
+  return next(clonedRequest);
 }
