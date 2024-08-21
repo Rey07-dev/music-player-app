ff605d4a8626e7b27077ad9db3d5b59d4d29c595
Reuben Ekow Yawson <87475624+Reuben-PropsDev@users.noreply.github.com>
2024-08-21 13:16:34 +0000

    feat: add a playlist content component

--- src/app/pages/playlists/playlists.component.ts
+++ src/app/pages/playlists/playlists.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SpotifyService } from '../../core/models/services/spotify/spotify.service';
+import { Playlist, Track, TrackItem } from '../../core/models/interfaces/playlist';
 
 @Component({
   selector: 'app-playlists',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrl: './playlists.component.css'
 })
 export class PlaylistsComponent {
+  playlists!: Playlist;
+  playlistImg!: string;
+  followers!: number;
+  tracks!: TrackItem[];
 
+  constructor( private spotifyService: SpotifyService) { }
+
+  ngOnInit(): void {
+
+    this.spotifyService.getPlaylist().subscribe({
+      next: (data) => {
+        if (data) {
+          this.playlists = data;
+          this.playlistImg = data?.images[0]?.url;
+          this.followers =  data?.followers?.total
+          this.tracks = data.tracks.items
+        }
+      },
+      error: (err) => {
+        console.error('Error fetching playlists', err);
+      }
+    });
+  }
 }
