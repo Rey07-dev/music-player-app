054b3a31d100385739bd810406f84c6ddcddfebd
Reuben Ekow Yawson <87475624+Reuben-PropsDev@users.noreply.github.com>
2024-08-21 12:46:25 +0000

    feat: add routes for playlist and spotifyLogin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,20 +3,24 @@ import { HomeComponent } from './pages/home/home.component';
 import { AlbumsComponent } from './pages/albums/albums.component';
 import { ArtistsComponent } from './pages/artists/artists.component';
 import { TrendingComponent } from './pages/trending/trending.component';
-import { LoginComponent } from './shared/components/login/login.component';
+import { SpotifyLoginComponent } from './shared/components/login/login.component';
 import { CallbackComponent } from './shared/components/login/callback.component';
 import { AuthGuard } from './core/models/guards/auth.guard';
 import { TracksInAlbumComponent } from './shared/components/tracks-in-album/tracks-in-album.component';
 import { GenreComponent } from './pages/genre/genre.component';
+import { PlaylistsComponent } from './pages/playlists/playlists.component';
+import { LoginComponent } from './shared/components/auth/login/login.component';
+import { SignupComponent } from './shared/components/auth/signup/signup.component';
 
 export const AppRoutes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full', canActivate: [AuthGuard] },
   { path: 'albums', component: AlbumsComponent, canActivate: [AuthGuard] },
   { path: 'album/:id', component: TracksInAlbumComponent, canActivate: [AuthGuard] },
-  { path: 'artists', component: ArtistsComponent, canActivate: [AuthGuard] },
+  { path: 'artist/:id', component: ArtistsComponent, canActivate: [AuthGuard] },
   { path: 'trending', component: TrendingComponent, canActivate: [AuthGuard] },
-  { path: 'login', component: LoginComponent },
   { path: 'genres', component: GenreComponent, canActivate: [AuthGuard] },
+  { path: 'playlists', component: PlaylistsComponent, canActivate: [AuthGuard] },
+  { path: 'spotify-login', component: SpotifyLoginComponent },
   { path: 'callback', component: CallbackComponent },
   { path: '**', redirectTo: '' },
 
