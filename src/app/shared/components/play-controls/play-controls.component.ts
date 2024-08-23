4fe65b7e522e30b9a1d95482741c4d76e729475b
Reuben Ekow Yawson <87475624+Reuben-PropsDev@users.noreply.github.com>
2024-08-23 09:49:56 +0000

    feat: add seek function

--- src/app/shared/components/play-controls/play-controls.component.ts
+++ src/app/shared/components/play-controls/play-controls.component.ts
@@ -1,9 +1,7 @@
-import { PlaybackService } from './../../../core/models/services/play/playback.service';
 import { Component, ElementRef, ViewChild } from "@angular/core";
-import { Store } from "@ngrx/store";
-
 import { SpotifyPlayerService } from "../../../core/models/services/spotify/spotify-player.service";
 import { PlayingSongState } from "../../../core/models/interfaces/spotify";
+import { interval, Subscription, switchMap } from "rxjs";
 
 @Component({
   selector: "app-play-controls",
@@ -11,38 +9,60 @@ import { PlayingSongState } from "../../../core/models/interfaces/spotify";
   styleUrl: "./play-controls.component.css",
 })
 export class PlayControlsComponent {
-
   @ViewChild("audioPlayerRef") audioPlayerRef!: ElementRef<HTMLAudioElement>;
   isPlaying: boolean = false;
   trackDetail!: PlayingSongState | null;
   artistName: any;
   playImage!: string;
+  currentTime: number = 0;
+  duration: number = 0;
+  private pollSubscription!: Subscription;
 
-  constructor(
-    private spotifyPlayerService: SpotifyPlayerService,
-    private store: Store, private playbackService: PlaybackService
-  ) {
-    this.spotifyPlayerService.initializePlayer();
-  }
+  constructor(private spotifyPlayerService: SpotifyPlayerService) {}
 
   ngOnInit(): void {
+    setTimeout(() => {
+      this.spotifyPlayerService.initializePlayer();
+    },2000)
+    this.updateTrackInfo();
+
+    this.pollSubscription = interval(1000).pipe(
+      switchMap(() => this.spotifyPlayerService.getCurrentlyPlayingTrack())
+    ).subscribe(trackDetails => {
+      if (trackDetails) {
+        this.currentTime = trackDetails.currentTime;
+        this.duration = trackDetails.duration;
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.pollSubscription.unsubscribe();
+  }
+
+  updateTrackInfo() {
     setInterval(() => {
       this.trackDetail = this.trackInfo();
-      this.playImage = this.trackDetail?.track_window.current_track.album.images[2].url || '';
-      this.artistName = this.trackDetail?.track_window.current_track.artists[0].name;
+      this.playImage =
+      this.trackDetail?.track_window.current_track.album.images[2].url || "";
+      this.artistName =
+      this.trackDetail?.track_window.current_track.artists[0].name;
     }, 100);
   }
 
-  trackInfo(){
-    const playerState = localStorage.getItem('player_state');
+
+  trackInfo() {
+    const playerState = localStorage.getItem("player_state");
     return playerState ? JSON.parse(playerState) : null;
   }
 
   play() {
-    const playlistUris: PlayingSongState = localStorage.getItem('player_state')
-     ? JSON.parse(localStorage.getItem('player_state')!)
-     : null;
-    this.spotifyPlayerService.play(playlistUris.track_window.current_track.album.uri);
+    const playlistUris: PlayingSongState = localStorage.getItem("player_state")
+      ? JSON.parse(localStorage.getItem("player_state")!)
+      : null;
+    this.spotifyPlayerService.play(
+      playlistUris.track_window.current_track.album.uri
+    );
   }
 
   pause() {
@@ -58,9 +78,15 @@ export class PlayControlsComponent {
     this.spotifyPlayerService.previous();
   }
 
-  onKeyPress(event: KeyboardEvent): void {}
+  seek(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const newTime = parseFloat(input.value);
+    this.spotifyPlayerService.seekToPosition(newTime);
+  }
 
-  onKeyDown(event: KeyboardEvent): void {}
+  onKeyPress(): void {}
 
-  onKeyUp(event: KeyboardEvent): void {}
+  onKeyDown(): void {}
+
+  onKeyUp(): void {}
 }
