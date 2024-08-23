db81caac24218c2563bed9bcb681ebd01c826111
Reuben Ekow Yawson <87475624+Reuben-PropsDev@users.noreply.github.com>
2024-08-23 09:52:32 +0000

    refactor: remove unused code

--- src/app/shared/components/tracks-in-album/tracks-in-album.component.ts
+++ src/app/shared/components/tracks-in-album/tracks-in-album.component.ts
@@ -55,11 +55,4 @@ export class TracksInAlbumComponent {
   getArtistNames(name: any) {
     return name?.artists[0].name;
   }
-  duration(duration: any) {
-    duration = Math.round(duration?.duration_ms / 1000);
-    const minutes = Math.floor((duration % 3600) / 60);
-    const seconds = duration % 60;
-    const formattedTime = `${minutes}:${seconds.toString().padStart(2, "0")}`;
-    return formattedTime;
-  }
 }
