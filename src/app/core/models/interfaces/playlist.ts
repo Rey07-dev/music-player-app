427d2192cb1cc00db3414c26930ad30baf182d0b
Reuben Ekow Yawson <87475624+Reuben-PropsDev@users.noreply.github.com>
2024-08-23 09:58:41 +0000

    feat: add interfaces

--- src/app/core/models/interfaces/playlist.ts
+++ src/app/core/models/interfaces/playlist.ts
@@ -39,3 +39,33 @@ export interface Image {
   url: string;
   width: number | null;
 }
+
+
+export interface SpotifyPlayerOptions {
+  name: string;
+  volume: number;
+}
+
+export interface SpotifyPlayerState {
+  __zone_symbol__state: boolean;
+}
+
+export interface SpotifyPlayer {
+  _options: SpotifyPlayerOptions;
+  _eventListeners: {
+    account_error: Function[];
+    authentication_error: Function[];
+    autoplay_failed: Function[];
+    playback_error: Function[];
+    initialization_error: Function[];
+    ready: (Function | null)[];
+    not_ready: Function[];
+    player_state_changed: (Function | null)[];
+    progress: Function[];
+  };
+  _connectionRequests: Record<string, unknown>;
+  _getCurrentStateRequests: Record<string, unknown>;
+  _getVolumeRequests: Record<string, unknown>;
+  _messageHandlers: Record<string, unknown>;
+  isLoaded: SpotifyPlayerState;
+}
